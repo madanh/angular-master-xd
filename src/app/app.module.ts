b1c7d5cb3dcfecf672f213b94a9af1c445a9a1a7
madanh <madanh2014@gmail.com>
2021-03-02 21:12:55 +0200

    Rough Posts component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+export interface Post {
+  
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor() { }
+}
